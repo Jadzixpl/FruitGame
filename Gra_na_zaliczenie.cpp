efb2ddfd2aaf577e41cade00d97b813c37b51cba
Jadwiga Zyzańska <jadziaz445@gmail.com>
2024-05-27 22:39:25 +0200

    naprawanaprawioneuwu

--- Gra_na_zaliczenie.cpp
+++ Gra_na_zaliczenie.cpp
@@ -22,6 +22,26 @@
 
 using namespace std;
 
+
+
+extern sf::Vector2f space1(155.f, 164.f), space2(155.f, 314.f), space3(155.f, 464.f), space4(814.f, 164.f), space5(814.f, 314.f), space6(814.f, 464.f);
+extern bool taken1 = false, taken2 = false, taken3 = false, taken4 = false, taken5 = false, taken6 = false;
+
+sf::Texture jablko;
+auto teksturaJablka = jablko.loadFromFile("apyl.png");
+
+sf::Texture gruszka;
+auto teksturaGruszki = gruszka.loadFromFile("gruszka.png");
+
+sf::Texture banan;
+auto teksturaBanana = banan.loadFromFile("banan.png");
+
+sf::Texture winogrona;
+auto teksturaWinogrona = winogrona.loadFromFile("winogrona.png");
+
+extern int kindOfFruit = rand() % 4 + 1; //owoce
+extern int placeForFruit = rand() % 6 + 1; //miejsca
+
 //podstawowe zmienne
 bool gameOver = true;
 long int totalScore = 0;
